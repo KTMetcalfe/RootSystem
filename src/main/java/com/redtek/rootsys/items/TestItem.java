ba6ec08aa225cad4df2e18c23c3d1c7fec93f273
KTMetcalfe <kmetcalfe4@gmail.com>
2020-02-05 19:10:45 -0600

    Added item tier

--- src/main/java/com/redtek/rootsys/items/TestItem.java
+++ src/main/java/com/redtek/rootsys/items/TestItem.java
@@ -10,6 +10,11 @@ import net.minecraftforge.common.ToolType;
 
 public class TestItem extends PickaxeItem{
     public TestItem() {
+
+        // Issue with getAttackDamage - attackDamage is defined as float in ItemTier but
+        // PickaxeItem takes attackDamage in as an int and then converts it to back a float
+        // This results in an incorrect value shown ingame for the item's attack damage
+
         super(Tiers.TEST, (int) Tiers.TEST.getAttackDamage(), 0, new Properties().group(ModItemGroups.MOD_ITEM_GROUP)
                 .maxStackSize(1)
         );
