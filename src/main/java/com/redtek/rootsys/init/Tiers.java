9ceeebcada0def41632fb0aba687e9703587157b
KTMetcalfe <kmetcalfe4@gmail.com>
2020-02-05 19:07:16 -0600

    Added item tier

--- src/main/java/com/redtek/rootsys/init/Tiers.java
+++ src/main/java/com/redtek/rootsys/init/Tiers.java
@@ -1,32 +1,36 @@
 package com.redtek.rootsys.init;
 
 import net.minecraft.item.IItemTier;
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.IItemProvider;
 import net.minecraft.util.LazyValue;
 
+import java.util.function.Supplier;
+
 public enum Tiers implements IItemTier {
-    TEST(3, 1561, 8.0F, 3.0F, 10, () -> {
-        return Ingredient.fromItems(Items.DIAMOND);
+    TEST(3, 1561, 10.0F, 3.0F, 15, () -> {
+        return Ingredient.fromItems(new IItemProvider[]{ModBlocks.TEST_ORE});
     });
 
+    private final int harvestLevel;
     private final int maxUses;
     private final float efficiency;
     private final float attackDamage;
     private final int enchantability;
-    private final int harvestLevel;
     private final LazyValue<Ingredient> repairMaterial;
 
-    Tiers(int maxUses, float efficiency, float attackDamage, int enchantability, int harvestLevel, LazyValue<Ingredient> repairMaterial) {
-        this.maxUses = maxUses;
-        this.efficiency = efficiency;
-        this.attackDamage = attackDamage;
-        this.enchantability = enchantability;
-        this.harvestLevel = harvestLevel;
-        this.repairMaterial = repairMaterial;
+    Tiers(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
+        this.harvestLevel = harvestLevelIn;
+        this.maxUses = maxUsesIn;
+        this.efficiency = efficiencyIn;
+        this.attackDamage = attackDamageIn;
+        this.enchantability = enchantabilityIn;
+        this.repairMaterial = new LazyValue(repairMaterialIn);
     }
 
+    public int getHarvestLevel() {
+        return this.harvestLevel;
+    }
 
     public int getMaxUses() {
         return this.maxUses;
@@ -38,10 +42,6 @@ public enum Tiers implements IItemTier {
 
     public float getAttackDamage() {
         return this.attackDamage;
-    }
-
-    public int getHarvestLevel() {
-        return this.harvestLevel;
     }
 
     public int getEnchantability() {
--- src/main/java/com/redtek/rootsys/items/TestItem.java
+++ src/main/java/com/redtek/rootsys/items/TestItem.java
@@ -1,18 +1,17 @@
 package com.redtek.rootsys.items;
 
 import com.redtek.rootsys.init.ModItemGroups;
+import com.redtek.rootsys.init.Tiers;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemTier;
 import net.minecraft.item.PickaxeItem;
 import net.minecraftforge.common.ToolType;
 
-public class TestItem extends PickaxeItem {
+public class TestItem extends PickaxeItem{
     public TestItem() {
-        super(ItemTier.DIAMOND, ItemTier.DIAMOND.getMaxUses(), 0, new Properties().group(ModItemGroups.MOD_ITEM_GROUP)
+        super(Tiers.TEST, (int) Tiers.TEST.getAttackDamage(), 0, new Properties().group(ModItemGroups.MOD_ITEM_GROUP)
                 .maxStackSize(1)
-                .maxDamage(2000)
-                .addToolType(ToolType.PICKAXE, 2)
         );
     }
 }
