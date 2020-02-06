871f19a51d39c97ab7009352d707099caba628a8
KTMetcalfe <kmetcalfe4@gmail.com>
2020-02-05 23:33:24 -0600

    Added 3x3 pickaxe

--- src/main/java/com/redtek/rootsys/RootSystem.java
+++ src/main/java/com/redtek/rootsys/RootSystem.java
@@ -1,5 +1,8 @@
 package com.redtek.rootsys;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.ICommandSource;
+import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,5 +16,10 @@ public class RootSystem {
 
   public RootSystem() {
     LOGGER.debug("Hello World!");
+  }
+
+  public static void sendMessage(String content) {
+    assert Minecraft.getInstance().player != null;
+    Minecraft.getInstance().player.sendChatMessage(content);
   }
 }
--- src/main/java/com/redtek/rootsys/items/TestAxe.java
+++ src/main/java/com/redtek/rootsys/items/TestAxe.java
@@ -2,7 +2,13 @@ package com.redtek.rootsys.items;
 
 import com.redtek.rootsys.init.ModItemGroups;
 import com.redtek.rootsys.init.Tiers;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class TestAxe extends AxeItem {
 
@@ -10,5 +16,14 @@ public class TestAxe extends AxeItem {
     super(Tiers.POO, (int) Tiers.POO.getAttackDamage(), 0, new Properties().group(ModItemGroups.MOD_ITEM_GROUP)
         .maxStackSize(1)
     );
+  }
+
+  public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+
+    for (int i = 0; i < 6; i++) {
+      //Vein Miner
+    }
+
+    return super.onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
   }
 }
