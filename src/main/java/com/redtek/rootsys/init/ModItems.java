6021bad0458536f755771b65d9fd05c1bcab85a0
KTMetcalfe <kmetcalfe4@gmail.com>
2020-02-08 11:10:36 -0600

    Added TODO to ModItems

--- src/main/java/com/redtek/rootsys/init/ModItems.java
+++ src/main/java/com/redtek/rootsys/init/ModItems.java
@@ -10,6 +10,25 @@ import net.minecraftforge.registries.ObjectHolder;
 @ObjectHolder(RootSystem.MODID)
 public class ModItems {
 
+  /* TODO
+  Add modified armor variants with textures, recipes, and items respective to modifiers
+
+  Ideas:
+    Rooted Armor: Strengthen armor, use sticks to make roots to add to base armor
+
+    Flarmor: "Flower Armor", with full setup, flower sword in hand becomes powerful
+
+    Enlighted Armor: Super resistant armor, knocks back attacking mobs, allows you to access enlightened block that dupes items for enlightened crystal
+
+    Enlightened Crystal: Used to craft enlightened block and create powerful armor
+
+    Roots: Created with axe and log and is used to modify base armor
+
+    Flower Sword: Cheap sword thats only useful with flower armor
+
+
+  */
+
   public static final Item TEST_PICKAXE = new TestPickaxe().setRegistryName(RootSystem.MODID, "test_pickaxe");
 
   public static final Item TEST_AXE = new TestAxe().setRegistryName(RootSystem.MODID, "test_axe");
